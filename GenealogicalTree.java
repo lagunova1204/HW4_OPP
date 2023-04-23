bfcf4364319da3eb1c3002d9d76c37544f3ede6b
lagunova1204 <lagunova.irina1204@gmail.com>
2023-04-23 20:17:47 +0300

    add parametrized and add class to communicate with the user

---
+++ GenealogicalTree.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public interface GenealogicalTree<T extends Person<T>> {
+    void addParent(T parent, T child);
+    void addSibling(T sibling1, T sibling2);
+    List<T> getChildren(T parent);
+}
